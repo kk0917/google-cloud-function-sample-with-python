6a0f2b6cda48ab22c86fce1a424d3974a47e224c
ko-kamenashi <kamenashi0917@gmail.com>
2020-10-06 12:49:43 +0900

    chore: delete garbage codes

--- src/sample2/main.py
+++ src/sample2/main.py
@@ -11,31 +11,21 @@ TABLE_ID   = os.environ['BIGQUERY_TABLE_ID']
 
 def main(request):
     req_params = setRequestParams(request)
-    result     = fetchMasterData(req_params) # ResultProxy obj
-    resp       = result.fetchall()         # list obj
+    result     = fetchMasterData(req_params) # return ResultProxy obj
+    resp       = result.fetchall()           # return list obj
 
     if 2 > len(resp) > 0:
         return convert_resp2json('master_db', resp)
     # elif len(resp) > 1:
     #     return json.dumps({"error": "duplicate data Exists..."}, indent=4)
     else:
-        dic = get_company_names_dic(request.args.get['target_name'])
-        return str(dic.total_count)
-        for row in dic:
-            return "{}".format(row['string_field_0'])
+        bq_result = get_company_names_dic(req_params['target_name'])
+        for row in bq_result:
+            return str(row)
         # return identify_company_name(req_params['target_name'])
+        # ...
 
-        to_json = ''
-
-        if len(result) == 1: # TODO: [and len(result) > 1:]?
-            to_json = result
-            insert(req_params.sys_id, req_params.sys_master_id, result.unique_name) # TODO: update
-        elif len(result) > 1: # TODO: enable marge with if?
-            to_json = result
-        else:
-            to_json = {"message": "couldn't find..."}
-        
-        return convert_resp2json('bigquery', to_json)
+        return convert_resp2json('_bigquery', to_json)
 
 def setRequestParams(request):
     return {
@@ -59,38 +49,38 @@ def convert_resp2json(reference, resp):
             }
 
             resp_dict.update(_dict)
-    elif reference == 'bigquery':
-        resp_dict.update({"error": "bigquery doesn\'t exists..."}) # ...TODO:
+    elif reference == '_bigquery':
+        for row in resp:
+            _dict = {
+                "string_field_0": row.string_field_0
+            }
+
+            resp_dict.update(_dict)
     else:
         resp_dict.update({"error": "Unknown reference"})
 
-    # TODO: investigate why dict type return json type even though converting json
-    # return json.dumps(resp_dict, ensure_ascii=False, indent=4)
-    return resp_dict
+    # return insert(request) # TODO: update after completing inprementation
 
 def identify_company_name(target_name):
     fmt_name_str = fmt_string(target_name)
     return eval_company_name(fmt_name_str)
 
 def fmt_string(target_name): # TODO: fix inappropriate variables name
-    _fmt_name_str = target_name
+    _fmt_name_str = 'aaa'
 
-    # ...
-
-    return True
+    return _fmt_name_str
 
 def eval_company_name(fmt_name_str):
-    company_names_dic  = get_company_names_dic()
+    fetchOne = ''
 
     # ...
 
-    return True
+    return fetchOne
 
-def get_company_names_dic():
+def get_company_names_dic(target_name):
     client    = bigquery.Client()
-    query_job = client.query('SELECT `column_name` FROM `project_name.dataset_name.table_name`')
+    # query_job = client.query("SELECT string_field_0 FROM `dac-techdev0.jcl_dic.jcl_dic` WHERE string_field_0 = {}".format(target_name))
+    query_job = client.query('SELECT string_field_0 FROM `dac-techdev0.jcl_dic.jcl_dic`')
+    result    = query_job.result()  # Waits for job to complete.
 
-    results = query_job.result()  # Waits for job to complete.
-
-    for row in results:
-        print("{} : {} views".fmt(row.url, row.view_count))
+    return result
